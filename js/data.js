058378eb1bb627ff2f1cebeb8a34d7f5ad9fe530
Lancer <lancer123987@gmail.com>
2020-08-20 16:53:06 +0800

    0820

--- js/data.js
+++ js/data.js
@@ -1,3 +1,14 @@
 $(document).ready(function () {
+    //初始化 firebase
+    var config = {
+        apiKey: "AIzaSyDlA_6VGaaEOogKMxFFaNvgyPUkBMsQfUg",
+        authDomain: "專案 firebaseapp 網址",
+        databaseURL: "專案資料庫網址",
+        projectId: "baseballsirius",
+        storageBucket: "storageBucket",
+        messagingSenderId: "messagingSenderId"
+    };
+    firebase.initializeApp(config);
 
+    var db = firebase.firestore();
 });
