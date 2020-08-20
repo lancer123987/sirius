3731ecd21a524c6d8006774ac8c9d94d14ff32fc
Lancer <lancer123987@gmail.com>
2020-08-20 17:05:17 +0800

    已可寫入

--- js/data.js
+++ js/data.js
@@ -1,14 +1,27 @@
 $(document).ready(function () {
     //初始化 firebase
-    var config = {
+    var firebaseConfig = {
         apiKey: "AIzaSyDlA_6VGaaEOogKMxFFaNvgyPUkBMsQfUg",
-        authDomain: "專案 firebaseapp 網址",
-        databaseURL: "專案資料庫網址",
+        authDomain: "baseballsirius.firebaseapp.com",
+        databaseURL: "https://baseballsirius.firebaseio.com",
         projectId: "baseballsirius",
-        storageBucket: "storageBucket",
-        messagingSenderId: "messagingSenderId"
+        storageBucket: "baseballsirius.appspot.com",
+        messagingSenderId: "254226143983",
+        appId: "1:254226143983:web:85852267eecb7dde"
     };
-    firebase.initializeApp(config);
+    firebase.initializeApp(firebaseConfig);
 
     var db = firebase.firestore();
+
+    function storedata() {
+        db.collection("SiriusMember").doc("002").set({
+            name: ["Lancer", "00"]
+        });
+    }
+
+    $('#submit').click(function () {
+        db.collection("SiriusMember").doc("002").set({
+            name: ["Lancer", "00"]
+        });
+    });
 });
