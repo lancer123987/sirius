7531dee1cc4e9964554976a53027923898097836
Lancer <lancer123987@gmail.com>
2020-08-28 02:37:03 +0800

    20200828

--- js/game_input.js
+++ js/game_input.js
@@ -9,24 +9,24 @@ $(document).ready(function() {
         var docName = gameType + date
             // var docName = docName.toString();
         console.log(docName);
-        db.collection("gameName").doc(ttt).set({
+        db.collection('gameName').doc(docName).set({
             league: league,
             place: place,
             adversary: adversary,
             weather: weather
         });
-        db.collection("game").doc(ttt).set({});
+        db.collection("game").doc(docName).set({});
     });
     // $('#submit').click(function() {
-    //     var num = $('#num').val();
-    //     var name = $('#name').val();
+    //     var pitcher = $('.pitcher').val();
+    //     var hobit_h = $('.hobit_h').val();
     //     var nick = $('#nick').val();
     //     var hobit_p = $('#hobit_p').val();
     //     var hobit_h = $('#hobit_h').val();
     //     var size_head = $('#size_head').val();
     //     var size_clothes = $('#size_clothes').val();
     //     db.collection("SiriusMember").doc(num).set({
-    //         name: [name, nick],
+    //         pitcher: pitcher,
     //         hobit: [hobit_p, hobit_h],
     //         size: [size_head, size_clothes],
     //     });
