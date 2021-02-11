ccacb95eefded9047888e3ba7f902bb571de92e6
Lancer <lancer123987@gmail.com>
2021-02-11 17:24:23 +0800

    0211

--- assets/js/common/vueData.js
+++ assets/js/common/vueData.js
@@ -1,3 +1,4 @@
+//主選單
 Vue.component('menu-item', {
     props: ['item'],
     template: '<li><a v-bind:href="item.listMenu[1]" v-bind:title="item.listMenu[0]">{{ item.listMenu[0] }}</a></li>'
@@ -10,6 +11,24 @@ var mainMenu = new Vue({
             { listMenu: ['Game', 'game.html'] },
             { listMenu: ['Member', 'member.html'] },
             { listMenu: ['About', 'about.html'] }
+        ]
+    }
+})
+
+//球員資料選單
+Vue.component('member-menu-item', {
+    props: ['item'],
+    template: '<li><a v-bind:href="item.listMenu[1]" v-bind:title="item.listMenu[0]"><img v-bind:src="item.listMenu[2]">{{ item.listMenu[0] }}</a></li>'
+})
+
+var mainMenu = new Vue({
+    el: '#memberAside',
+    data: {
+        menu: [
+            { listMenu: ['Basic', '#', 'assets/images/common/icon_member_white.svg'] },
+            { listMenu: ['Attack', '#', 'assets/images/common/icon_attack_white.svg'] },
+            { listMenu: ['Pitcher', '#', 'assets/images/common/icon_baseball_white.svg'] },
+            { listMenu: ['Defend', '#', 'assets/images/common/icon_glove_white.svg'] }
         ]
     }
 })
