e475d7bd8860f90684af6474c518bb6f631d0353
Lancer <lancer123987@gmail.com>
2021-02-12 00:05:48 +0800

    0212

--- assets/js/common/vueData.js
+++ assets/js/common/vueData.js
@@ -14,21 +14,3 @@ var mainMenu = new Vue({
         ]
     }
 })
-
-//球員資料選單
-Vue.component('member-menu-item', {
-    props: ['item'],
-    template: '<li><a v-bind:href="item.listMenu[1]" v-bind:title="item.listMenu[0]"><img v-bind:src="item.listMenu[2]">{{ item.listMenu[0] }}</a></li>'
-})
-
-var mainMenu = new Vue({
-    el: '#memberAside',
-    data: {
-        menu: [
-            { listMenu: ['Basic', '#', 'assets/images/common/icon_member_white.svg'] },
-            { listMenu: ['Attack', '#', 'assets/images/common/icon_attack_white.svg'] },
-            { listMenu: ['Pitcher', '#', 'assets/images/common/icon_baseball_white.svg'] },
-            { listMenu: ['Defend', '#', 'assets/images/common/icon_glove_white.svg'] }
-        ]
-    }
-})
