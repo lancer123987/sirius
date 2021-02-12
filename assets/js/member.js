5082586691831da0195e56b9294824d051ec681d
Lancer <lancer123987@gmail.com>
2021-02-13 00:20:23 +0800

    0213

--- assets/js/member.js
+++ assets/js/member.js
@@ -1,3 +1,11 @@
-$(document).ready(function () {
+$(document).ready(function() {
+    var db = firebase.firestore();
+    var ref = db.collection('SiriusMember');
 
+    ref.get().then(querySnapshot => {
+        querySnapshot.forEach(doc => {
+            //console.log(doc.id, doc.data());
+            console.log(doc.id);
+        });
+    });
 });
