35a65708e8a645820742a112946b66a3a7ec60e4
Lancer <lancer123987@gmail.com>
2021-02-19 00:39:09 +0800

    0219

--- behind/assets/js/game_input.js
+++ behind/assets/js/game_input.js
@@ -19,70 +19,96 @@ $(document).ready(function() {
     })
 
     /**讀取資料 & 選項渲染**/
-    var ref = db.collection('game');
+    var ref = db.collection('gameType');
     ref.get().then(querySnapshot => {
         querySnapshot.forEach(doc => {
             $('#gameType').append('<option value=\"' + doc.id + '\">' + doc.id + '<\/option>');
         });
     });
 
+    var placeRef = db.collection('code').doc('place');
+    placeRef.get().then(doc => {
+        let place = doc.data().place;
+        for (var i = 0; i < place.length; i++) {
+            $('#place').append('<option value=\"' + place[i] + '\">' + place[i] + '<\/option>')
+        }
+    });
+
     $('#gameType').change(function() {
+        $('#gameYear').find('option').remove();
         let league = $(this).val();
-        var ref = db.collection('game').doc(league);
+        var ref = db.collection('gameType').doc(league);
         ref.get().then(doc => {
-            //querySnapshot.forEach(collection => {
-            console.log(collection.id);
-            // $('#gameYear').append('<option value=\"' + collection.id + '\">' + collection.id + '<\/option>');
-            //});
+            let gameYear = doc.data().year;
+            for (var i = gameYear.length - 1; i > -1; i--) {
+                $('#gameYear').append('<option value=\"' + gameYear[i] + '\">' + gameYear[i] + '<\/option>')
+            }
         });
         $('#gameYear').show();
     });
+
+
     /**寫入資料**/
 
     //比賽基本資訊
     $('.gameData__submit').click(function() {
-        var date = $('.date').val();
-        var gameType = $('#gameType').val();
-        var frequency = $('.frequency').val();
-        var league = $('.league').val();
-        var place = $('.place').val();
-        var adversary = $('.adversary').val();
-        var weather = $('.weather').val();
-        var docName = gameType + date + '-' + frequency;
-        console.log(docName);
-        db.collection('gameName').doc(docName).set({
-            league: league,
-            place: place,
-            adversary: adversary,
-            weather: weather
+        let date = $('.date').val(); //日期
+        let gameType = $('#gameType').val(); //比賽類型
+        let gameYear = $('#gameYear').val(); //比賽年度
+        let frequency = $('.frequency').val(); //單日比賽場次
+        let docName = date + '-' + frequency; //比賽id建立
+        let place = $('.place').val(); //場地名稱
+        let adversary = $('.adversary').val(); //對手名稱
+        let home = $('.home').val(); //主客場
+        if ('true' == home) {
+            home = true;
+        } else {
+            home = false;
+        }
+        let weather = $('.weather').val(); //天氣
+
+        db.collection("game").doc(docName).set({
+            比賽聯盟: gameType,
+            比賽年度: gameYear,
+            場地: place,
+            對手: adversary,
+            天氣: weather,
+            主客場: home
         });
-        db.collection("game").doc(docName).set({});
         $('.gameData').hide();
         $('.game').fadeIn();
     });
 
     //比賽內容
     $('.game_submit').click(function() {
-        var date = $('.date').val();
-        var gameType = $('#gameType').val();
-        var frequency = $('.frequency').val();
-        var docName = gameType + date + '-' + frequency;
-        //
-        var pitcher = $('.pitcher').val();
-        var hobit_h = $('.hobit_h').val();
-        var hit = $('.hit').val();
-        var situation = $('.situation').val();
-        var result = $('.result').val();
-        var direction = $('.direction').val();
-        var hit_type = $('.hit_type').val();
-        db.collection("game").doc(docName).update({
-            pitcher: pitcher,
-            hobit_h: hobit_h,
-            hit: hit,
-            situation: situation,
-            result: result,
-            direction: direction,
-            hit_type: hit_type
+        let date = $('.date').val(); //日期
+        let frequency = $('.frequency').val(); //單日比賽場次
+        let docName = date + '-' + frequency; //比賽id建立
+
+        console.log(docName);
+
+        let round = $('.round').val(); //當前局數
+        let roundTurn = $('.round__turn').val(); //上下半局
+
+        let roundName = roundTurn + round;
+
+        let pitcher = $('.pitcher').val(); //投手背號
+        let hobit_h = $('.hobit_h').val(); //打者左右打
+        let hit = $('.hit').val(); //打者背號
+        let process = $('.process').val(); //過程
+        let result = $('.result').val(); //結果
+        let direction = $('.direction').val(); //方向
+        let hit_type = $('.hit_type').val(); //擊出結果
+        let remark = $('.remark').val(); //備註
+        db.collection("game").doc(docName).collection(roundName).doc('hitter01').set({
+            投手: pitcher,
+            左右打: hobit_h,
+            打者: hit,
+            過程: process,
+            結果: result,
+            方向: direction,
+            擊出結果: hit_type,
+            備註: remark
         });
     });
 });
