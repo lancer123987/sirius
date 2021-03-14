df2b728561a69cb5fa15f18ebd3d7f65ac9f7968
Lancer <lancer123987@gmail.com>
2021-03-14 15:58:30 +0800

    0314

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,5 +1,26 @@
 $(document).ready(function() {
-    // $('.game__content__title').click(function() {
-    //     $(this).next('.game__content__inner').stop().slideToggle();
-    // });
+    var ref = db.collection('game');
+    let boxGame = [];
+    let boxEnemy = [];
+    let boxPlace = [];
+    let boxScore = [];
+    let boxHome = [];
+    ref.get().then(querySnapshot => {
+        querySnapshot.forEach(doc => {
+            boxGame.push(doc.id);
+            boxEnemy.push(doc.data().對手);
+            boxPlace.push(doc.data().場地);
+            boxScore.push(doc.data().比數);
+            boxHome.push(doc.data().主客場);
+            let gameDate = doc.id.replaceAll('-', '/');
+            gameDate = gameDate.slice(0, gameDate.length - 2);
+
+            if (doc.data().主客場 == false) {
+                $('#game').append('<a class=\"game__item\" href=\"game_detail.html?' + doc.id + '\"><p class=\"game__item__team\"><span class=\"game__item__team__away\">Sirius</span>VS<span class=\"game__item__team__home\">' + doc.data().對手 + '</span></p><p class=\"game__item__score\">' + doc.data().比數 + '</p><p class=\"game__item__place\">' + doc.data().場地 + '</p><p class=\"game__item__date\">' + gameDate + '</p></a>');
+            } else {
+                $('#game').append('<a class=\"game__item\" href=\"game_detail.html?' + doc.id + '\"><p class=\"game__item__team\"><span class=\"game__item__team__away\">' + doc.data().對手 + '</span>VS<span class=\"game__item__team__home\">Sirius</span></p><p class=\"game__item__score\">' + doc.data().比數 + '</p><p class=\"game__item__place\">' + doc.data().場地 + '</p><p class=\"game__item__date\">' + gameDate + '</p></a>');
+            }
+        });
+        $('.c-loading').fadeOut();
+    });
 });
