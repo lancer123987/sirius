fe2fdda2d96cf6a054c2e5daa1b9d2bf68c8f43d
Lancer <lancer123987@gmail.com>
2021-03-16 22:54:01 +0800

    0316

--- assets/js/member.js
+++ assets/js/member.js
@@ -3,15 +3,15 @@ $(document).ready(function() {
     let boxData = [];
     ref.get().then(querySnapshot => {
         querySnapshot.forEach(doc => {
-            boxNumber = doc.id;
+            numberId = doc.id;
             boxData.push(doc.data().name[1]);
 
-            boxNumber = parseInt(boxNumber);
+            boxNumber = parseInt(numberId);
             if (boxNumber < 10) {
                 boxNumber = "0" + boxNumber;
             } else {}
 
-            $('#member .member__inner').append('<a class=\"member__inner__item\" href=\"data.html?member' + boxNumber + '\"><figure class=\"member__inner__item__pic\"><img src="assets/images/data/pic.jpg\"></figure><div class=\"member__inner__item__text\"><p class=\"member__inner__item__text__number\">' + boxNumber + '</p><p class=\"member__inner__item__text__name\">' + doc.data().name[1] + '</p></div></a>');
+            $('#member .member__inner').append('<a class=\"member__inner__item\" href=\"data.html?member' + numberId + '\"><figure class=\"member__inner__item__pic\"><img src="assets/images/data/pic.jpg\"></figure><div class=\"member__inner__item__text\"><p class=\"member__inner__item__text__number\">' + boxNumber + '</p><p class=\"member__inner__item__text__name\">' + doc.data().name[1] + '</p></div></a>');
         });
         $('.c-loading').fadeOut();
     });
