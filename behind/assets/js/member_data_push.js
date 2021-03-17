da8f0d9214834ba8f7e91c5c610d94ac9f081ceb
Lancer <lancer123987@gmail.com>
2021-03-17 21:46:48 +0800

    0317

--- behind/assets/js/member_data_push.js
+++ behind/assets/js/member_data_push.js
@@ -1,3 +1,20 @@
-$(document).ready(function () {
+$(document).ready(function() {
+    /**讀取資料 & 選項渲染**/
 
+    //場次
+    var ref = db.collection('game');
+    ref.get().then(querySnapshot => {
+        querySnapshot.forEach(doc => {
+            console.log(doc.id);
+            $('select[name="game_date"]').append('<option value=\"' + doc.id + '\">' + doc.id + '<\/option>');
+        });
+    });
+
+    //場次
+    var ref = db.collection('SiriusMember');
+    ref.get().then(querySnapshot => {
+        querySnapshot.forEach(doc => {
+            $('select[name="game_member"]').append('<option value=\"' + doc.id + '\">' + doc.id + '<\/option>');
+        });
+    });
 });
